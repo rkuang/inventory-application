d6ba4a49e9bf2c95e33328e19314c23a56dc3496
Ricky Kuang <rkuang26@gmail.com>
2020-12-21 16:39:51 -0800

    add view and update controller for category list GET action

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,6 +2,13 @@ const Category = require('../models/category');
 
 module.exports = {
   category_list: (req, res, next) => {
-    res.send('GET /categories not implemented.')
+    Category.find((err, categories) => {
+      if (err) next(err);
+      console.log(categories);
+      res.render('category_list', {
+        title: 'Category List',
+        categories: categories
+      });
+    })
   }
 }
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -4,10 +4,9 @@ module.exports = {
   item_list: (req, res, next) => {
     Item.find().populate('category').exec((err, items) => {
       if (err) next(err);
-      console.log(items[0].category._id)
       res.render('item_list', {
         title: 'Item List',
-        item_list: items
+        items: items
       });
     })
   }
