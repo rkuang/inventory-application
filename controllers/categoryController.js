8b790749a0fa3208f9471c129d9ab8f84891f64b
Ricky Kuang <rkuang26@gmail.com>
2020-12-23 14:40:15 -0800

    update delete GET and POST for category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -64,5 +64,66 @@ module.exports = {
         });
       }
     }
+  ],
+  category_delete_get: (req, res, next) => {
+    async.parallel({
+      category: (cb) => {
+        Category.findById(req.params.id, cb);
+      },
+      category_items: (cb) => {
+        Item.find({ 'category': req.params.id }, cb);
+      }
+    }, (err, results) => {
+      if (err) next(err);
+      if (results.category == null) {
+        const error = new Error('Category not found');
+        error.status = 404;
+        next(error);
+      }
+      res.render('category_delete', {
+        title: 'Delete Category',
+        category: results.category,
+        category_items: results.category_items
+      });
+    });
+  },
+  category_delete_post: (req, res, next) => {
+    Category.findByIdAndDelete(req.params.id, (err) => {
+      if (err) next(err);
+      res.redirect('/categories');
+    })
+  },
+  category_update_get: (req, res, next) => {
+    Category.findById(req.params.id, (err, category) => {
+      res.render('category_form', {
+        title: 'Update Category',
+        category: category
+      });
+    })
+  },
+  category_update_post: [
+    body('name', 'Name cannot be empty.').trim().not().isEmpty().escape(),
+    body('description', 'Description cannot be empty.').trim().not().isEmpty().escape(),
+    (req, res, next) => {
+      const errors = validationResult(req);
+      const category = new Category({
+        name: req.body.name,
+        description: req.body.description,
+        _id: req.params.id
+      });
+      if (!errors.isEmpty()) {
+        res.render('category_form', {
+          title: 'Update Category',
+          category: category,
+          errors: errors.array()
+        });
+      } else {
+        Category.findByIdAndUpdate(req.params.id, category, (err) => {
+          if (err) next(err);
+          res.redirect(category.url);
+        })
+      }
+    }
   ]
+
 }
--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,9 @@ router.get('/categories', categoryController.category_list);
 router.get('/category/create', categoryController.category_create_get);
 router.post('/category/create', categoryController.category_create_post)
 router.get('/category/:id', categoryController.category_detail);
-
+router.get('/category/:id/delete', categoryController.category_delete_get);
+router.post('/category/:id/delete', categoryController.category_delete_post);
+router.get('/category/:id/update', categoryController.category_update_get);
+router.post('/category/:id/update', categoryController.category_update_post);
 
 module.exports = router;
